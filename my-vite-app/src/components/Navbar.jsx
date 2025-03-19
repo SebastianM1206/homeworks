3aedafecfd486415e78f87dad738b713ee8d23ba
Sebastian <semega1206@gmail.com>
2025-03-19 17:59:08 -0500

    Navbar change

--- my-vite-app/src/components/Navbar.jsx
+++ my-vite-app/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           <Link to="/" className="text-2xl font-bold">
-            CHALLENGE '08'
+            CHALLENGE '09'
           </Link>
         </div>
       </div>
